6fe7acfff8eb66556a4bd2b43ba8ae9899d398b1
adriandcoding <theamr91@gmail.com>
2024-11-14 13:52:47 +0100

    Deleting useful archives

--- vite.config 2.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  root: 'src', // Define 'src' como el directorio raíz
-  build: {
-    outDir: '../dist', // Define el directorio de salida de la compilación fuera de 'src'
-  },
-});
